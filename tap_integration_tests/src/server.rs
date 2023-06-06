243ac7c5152b2a945f18cf07009cb1639f5ba3e7
Severiano Sisneros <severiano@semiotic.ai>
2023-06-06 09:43:26 -0700

    Added comments

--- tap_integration_tests/src/server.rs
+++ tap_integration_tests/src/server.rs
@@ -20,29 +20,43 @@ use tap_core::{
     tap_receipt::ReceiptCheck,
 };
 
+/// Rpc trait represents a JSON-RPC server that has a single async method `request`.
+/// This method is designed to handle incoming JSON-RPC requests.
 #[rpc(server)]
 pub trait Rpc {
+    // This async method is designed to handle incoming JSON-RPC requests.
     #[method(name = "request")]
     async fn request(
         &self,
-        request_id: u64,
-        receipt: SignedReceipt,
-    ) -> Result<(), jsonrpsee::types::ErrorObjectOwned>;
+        request_id: u64, // Unique identifier for the request
+        receipt: SignedReceipt, // Signed receipt associated with the request
+    ) -> Result<(), jsonrpsee::types::ErrorObjectOwned>; // The result of the request, a JSON-RPC error if it fails
 }
 
+/// RpcManager is a struct that implements the `Rpc` trait and it represents a JSON-RPC server manager.
+/// It includes a manager, initial_checks, receipt_count, threshold and aggregator_client.
+/// Manager holds a Mutex-protected instance of a generic `Manager` object which is shared and can be accessed by multiple threads.
+/// initial_checks is a list of checks that needs to be performed for every incoming request.
+/// receipt_count is a thread-safe counter that increments with each receipt verified and stored.
+/// threshold is a limit to which receipt_count can increment, after reaching which RAV request is triggered.
+/// aggregator_client is an HTTP client used for making JSON-RPC requests to another server.
 pub struct RpcManager<
-    CA: CollateralAdapter + Send + 'static,
-    RCA: ReceiptChecksAdapter + Send + 'static,
-    RSA: ReceiptStorageAdapter + Send + 'static,
-    RAVSA: RAVStorageAdapter + Send + 'static,
+    CA: CollateralAdapter + Send + 'static,  // An instance of CollateralAdapter, marked as thread-safe with Send and given 'static lifetime
+    RCA: ReceiptChecksAdapter + Send + 'static, // An instance of ReceiptChecksAdapter
+    RSA: ReceiptStorageAdapter + Send + 'static, // An instance of ReceiptStorageAdapter
+    RAVSA: RAVStorageAdapter + Send + 'static, // An instance of RAVStorageAdapter
 > {
-    manager: Arc<Mutex<Manager<CA, RCA, RSA, RAVSA>>>,
-    initial_checks: Vec<ReceiptCheck>,
-    receipt_count: Arc<AtomicU64>,
-    threshold: u64,
-    aggregator_client: HttpClient,
+    manager: Arc<Mutex<Manager<CA, RCA, RSA, RAVSA>>>, // Manager object in a mutex for thread safety, reference counted with an Arc
+    initial_checks: Vec<ReceiptCheck>, // Vector of initial checks to be performed on each request
+    receipt_count: Arc<AtomicU64>, // Thread-safe atomic counter for receipts
+    threshold: u64, // The count at which a RAV request will be triggered
+    aggregator_client: HttpClient, // HTTP client for sending requests to the aggregator server
 }
 
+
+/// Implementation for `RpcManager`, includes the constructor and the `request` method.
+/// Constructor initializes a new instance of `RpcManager`.
+/// `request` method handles incoming JSON-RPC requests and it verifies and stores the receipt from the request.
 impl<
         CA: CollateralAdapter + Send + 'static,
         RCA: ReceiptChecksAdapter + Send + 'static,
@@ -136,15 +150,16 @@ impl<
     }
 }
 
+/// request_rav function creates a request for aggregate receipts (RAV), sends it to another server and verifies the result.
 async fn request_rav<
     CA: CollateralAdapter + Send + 'static,
     RCA: ReceiptChecksAdapter + Send + 'static,
     RSA: ReceiptStorageAdapter + Send + 'static,
     RAVSA: RAVStorageAdapter + Send + 'static,
 >(
-    manager: Arc<Mutex<Manager<CA, RCA, RSA, RAVSA>>>,
-    time_stamp_buffer: u64,
-    aggregator_client: HttpClient,
+    manager: Arc<Mutex<Manager<CA, RCA, RSA, RAVSA>>>, // Mutex-protected manager object for thread safety
+    time_stamp_buffer: u64, // Buffer for timestamping, see tap_core for details
+    aggregator_client: HttpClient, // HttpClient for making requests to the tap_aggregator server
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Create the aggregate_receipts request params
     let rav = manager
@@ -164,21 +179,22 @@ async fn request_rav<
     Ok(())
 }
 
+/// run_server function initializes and starts a JSON-RPC server that handles incoming requests.
 pub async fn run_server<
     CA: CollateralAdapter + Send + 'static,
     RCA: ReceiptChecksAdapter + Send + 'static,
     RSA: ReceiptStorageAdapter + Send + 'static,
     RAVSA: RAVStorageAdapter + Send + 'static,
 >(
-    port: u16,
-    collateral_adapter: CA,
-    receipt_checks_adapter: RCA,
-    receipt_storage_adapter: RSA,
-    rav_storage_adapter: RAVSA,
-    initial_checks: Vec<ReceiptCheck>,
-    required_checks: Vec<ReceiptCheck>,
-    threshold: u64,
-    aggregate_server_address: String,
+    port: u16, // Port on which the server will listen
+    collateral_adapter: CA, // CollateralAdapter instance
+    receipt_checks_adapter: RCA, // ReceiptChecksAdapter instance
+    receipt_storage_adapter: RSA, // ReceiptStorageAdapter instance
+    rav_storage_adapter: RAVSA, // RAVStorageAdapter instance
+    initial_checks: Vec<ReceiptCheck>, // Vector of initial checks to be performed on each request
+    required_checks: Vec<ReceiptCheck>, // Vector of required checks to be performed on each request
+    threshold: u64, // The count at which a RAV request will be triggered
+    aggregate_server_address: String, // Address of the aggregator server
 ) -> Result<(ServerHandle, std::net::SocketAddr)> {
     // Setting up the JSON RPC server
     println!("Starting server...");
@@ -203,6 +219,7 @@ pub async fn run_server<
     Ok((handle, addr))
 }
 
+/// get_current_timestamp_u64_ns function returns current system time since UNIX_EPOCH as a 64-bit unsigned integer.
 fn get_current_timestamp_u64_ns() -> Result<u64, Error> {
     Ok(SystemTime::now()
         .duration_since(UNIX_EPOCH)
